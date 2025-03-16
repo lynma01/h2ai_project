a64bca6ee2903ecf2e9ccf50f4da0bfc34b744e0
Matthew Lyn <lynma01@gmail.com>
2025-03-16 08:01:50 -0400

    update readme

--- app.py
+++ app.py
@@ -19,16 +19,13 @@ def main():
 
     with col2:
         st.write("""
-        Orderli is a cutting-edge application designed for hospital risk managers. By leveraging machine learning, it predicts providers’ risk of complication and suggests optimal scheduling adjustments. This ensures that the right providers are assigned to the right procedures, maximizing patient safety and clinical efficacy.
+        Orderli is a AI/ML powered application designed to assist hospital risk managers predict providers’ risk of complication and suggest scheduling adjustments. This ensures that the right providers are assigned to the right schedules for their procedures, maximizing patient safety and clinical efficacy.
 
         - **Optimized Scheduling:**  
         By aligning provider risk with procedure scheduling, the app ensures that patients receive care from providers best suited for the procedure. This optimization leads to higher treatment efficacy and a reduction in complications.
 
         - **Proactive Risk Identification:**  
-        The machine learning model continuously assesses risk, identifying providers who may present a higher likelihood of complications. This allows for early intervention before risks translate into actual safety issues.
-
-        - **Reduction of Complication Rates:**  
-        By suggesting rescheduling of certain procedures based on risk prediction, Orderli directly contributes to minimizing complications. This systematic approach to risk management plays a key role in enhancing patient safety.
+        The machine learning model continuously assesses risk, identifying providers' schedules that may present a higher likelihood of complications. This enhances patient safety by allowing for early intervention before risks translate into actual safety issues.
 
         - **Improved Resource Management:**  
         The app aids in allocating provider resources more effectively, ensuring that the most suitable staff are scheduled for procedures. This targeted approach helps mitigate risks and enhances operational efficiency.
